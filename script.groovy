1c4946c24fee98e558470638f93c55d1c1814d29
Chikaodi-cloud <chikaodicloud@gmail.com>
2026-01-10 12:45:39 +0300

    Refactor build and deployment functions in script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,38 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-} 
-
+def buildApp() {
+    echo "Building the application..."
+    // Add build logic here
+}
+def testApp() {
+    echo "Testing the application..."
+    // Add test logic here
+}
 def deployApp() {
-    echo 'deploying the application...'
-} 
+    echo 'Deploying the application...'
+    echo "Deploying version: ${params.VERSION}"
+    // Add deploy logic here
+}
 
 return this
+
+
+
+
+// def buildJar() {
+//     echo "building the application..."
+//     sh 'mvn package'
+// } 
+
+// def buildImage() {
+//     echo "building the docker image..."
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push nanajanashia/demo-app:jma-2.0'
+//     }
+// } 
+
+// def deployApp() {
+//     echo 'deploying the application...'
+// } 
+
+// return this
